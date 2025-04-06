41b2348be977fcbcfd21d30f7d65fc532adc15b1
sarbadeep31 <sarbadeep3108@gmail.com>
2025-04-06 20:32:27 +0530

    Delete python__projects/Password_Generator.py

--- python__projects/Password_Generator.py
+++
@@ -1,9 +0,0 @@
-import random
-
-chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRlawidjcLJHEBSTUVWXYZ1234567890!@#$%^&*()?><:;}{]"
-
-user = int(input("enter length : "))
-
-for x in range(user):
-     password+=(random.choice(chars))
-print(password)    
